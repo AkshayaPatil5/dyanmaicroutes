95135602f40bfbe8e6660b587325203381bf274f
AkshayaPatil5 <akshayapatil1987@gmail.com>
2023-08-04 16:46:29 +0530

    connecting DB

---
+++ util/database.js
@@ -0,0 +1,10 @@
+const mysql =require('mysql2');
+const pool= mysql.createPool({
+host: 'localhost',
+user:'root',
+database:'node-complete',
+password:'Ak7558534825@'
+});
+
+
+module.exports = pool.promise();
