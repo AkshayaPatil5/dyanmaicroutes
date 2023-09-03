0124053dd4018fd08936599e1888a8dc8d912107
AkshayaPatil5 <akshayapatil1987@gmail.com>
2023-09-03 16:26:22 +0530

    association

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 
 const sequelize=require('./util/database');
+const Product=require('./models/product');
+const User=require('./models/user');
+
 
 const app = express();
 
@@ -20,18 +23,36 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use((req,res,next)=>{
+    User.findByPk(1).then(user=>{
+        req.user=user;
+        next();
+    }).catch(err=>console.log(err));
+});
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User,{constraints:true,onDelete:'CASCADE'});
+
+User.hasMany(Product);
+
 sequelize
-.sync().
-then(result=>{
+.sync({force:true})
+.then(result=>{
+    return User.findByPk(1);
     //console.log(result);
-    app.listen(4000);
+}).then(user=>{
+    if(!user){
+    User.create({name:'aksha',email:'aksh@123gmail.com'});
+}
+return user;
 })
+    .then(user=>{
+        //console.log(user)
+        app.listen(4000);
+    })
 .catch(err=>{
     console.log(err);
 });
